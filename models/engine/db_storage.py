f5ad3bbee066be79f3e09022257016144fe37f93
vick-ram <vickramodero6@gmail.com>
2024-05-27 13:22:07 -0700

    code refactor

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,14 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """returns the object based on class and its IDor None if not found"""
+	if cls and id:
+            fetch = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+        return all_obj.get(fetch)
+
+    def count(self, cls=None):
+        """method to count the number of objects in storage"""
+        return (len(self.all(cls)))
